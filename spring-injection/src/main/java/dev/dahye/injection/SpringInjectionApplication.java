473c585d7ed0804d7ac71e4df5c0995e4f020f7f
김다혜 <dahye432111@gmail.com>
2020-06-20 22:22:45 +0900

    initial project

---
+++ spring-injection/src/main/java/dev/dahye/injection/SpringInjectionApplication.java
@@ -0,0 +1,13 @@
+package dev.dahye.injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInjectionApplication.class, args);
+    }
+
+}
